3d6d067847633fe40eaae8c01a298dadbffaee17
dev <dev@local>
2026-04-21 23:07:12 +0530

    complete code

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import TralierBox from './components/tralier-box.jsx'
+
+function App() {
+  
+
+  return (
+   < TralierBox/>
+    
+  )
+}
+
+export default App
